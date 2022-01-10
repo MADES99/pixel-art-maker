85c93527a8f6234e610badcdb22c92bdb5b68fea
Salam Mubarak <bnabdussalam@gmail.com>
2022-01-11 00:50:11 +0100

    Udacity Project

---
+++ designs.js
@@ -0,0 +1,44 @@
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+
+// function makeGrid() {
+
+// Your code goes here!
+
+"use strict";
+
+const $tableElement = $('#pixelCanvas');
+const $inputHeight = $('#inputHeight');
+const $inputWidth = $('#inputWidth');
+const $colorPicker = $('#colorPicker');
+
+$('#sizePicker').submit( event => {
+    event.preventDefault();
+
+    let width = $inputWidth.val();
+    let height = $inputHeight.val();
+
+    $tableElement.html(''); //clear
+
+    makeGrid(height, width);
+    addCellClickListener();
+});
+
+function makeGrid(height, width) {
+    for(let i = 0; i < height; i++) {
+        $tableElement.append('<tr></tr>');
+    };
+
+    for(let i = 0; i < width; i++) {
+        $('tr').append('<td></td>');
+    };
+};
+
+function addCellClickListener() {
+    $('td').click( event => {
+        let color = $colorPicker.val();
+        $(event.currentTarget).css("background-color", color)
+    });
+};
